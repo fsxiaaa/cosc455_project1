1961a8badc8ea3faee891c3420fb6fd96e9fad26
fsxiaaa <ftchen1@students.towson.edu>
2015-10-29 22:18:54 -0400

    Updated Compiler.

--- src/edu/towson/cis/cosc455/ftchen/project1/implementation/MyCompiler.java
+++ src/edu/towson/cis/cosc455/ftchen/project1/implementation/MyCompiler.java
@@ -1,12 +1,57 @@
 package edu.towson.cis.cosc455.ftchen.project1.implementation;
 
+import java.io.*;
+
 public class MyCompiler {
 
 	public static String currentToken = "";
+	public static String nextToken = "";
+	//MyLexicalAnalyzer lexicalAnalyzer = new MyLexicalAnalyzer();
+	//MySyntaxAnalyzer syntaxAnalyzer = new MySyntaxAnalyzer();
+	//MySemanticAnalyzer semanticAnalyzer = new MySemanticAnalyzer();
 	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("runs");
+		
+		MyLexicalAnalyzer lexicalAnalyzer = new MyLexicalAnalyzer();
+		MySyntaxAnalyzer syntaxAnalyzer = new MySyntaxAnalyzer();
+		MySemanticAnalyzer semanticAnalyzer = new MySemanticAnalyzer();
+		
+		if (args[0].substring(args[0].length()-4,args[0].length()).equals(".mkd")){
+			//VALID FILE - READ FROM FILE
+			FileReader fr;
+			BufferedReader br;
+			try {
+				fr = new FileReader(args[0]);
+				br = new BufferedReader(fr);
+				String sourceLine = null;
+				while ((sourceLine = br.readLine()) != null){
+					//Call Lexical Analyzer
+					lexicalAnalyzer.start(sourceLine);
+				}
+				
+			} catch (FileNotFoundException e) {
+				System.out.println("Error. File not found.");
+				System.exit(0);
+			} catch (IOException e) {
+				System.out.println("Error. I/O Exception occurred. Failed to read from file.");
+				System.exit(0);
+			}
+			
+			
+			//Call Lexical Analyzer
+			lexicalAnalyzer.getNextToken();
+			System.out.println("Lexical Analyzer");
+		}
+		else{
+			//INVALID FILE
+			System.out.println("Error, invalid file. File extension must be .mkd");
+		}
+		
+		System.out.println(args[0]);
+		
+		System.out.println("still runs");
 	}
 
 }
--- src/edu/towson/cis/cosc455/ftchen/project1/implementation/MyLexicalAnalyzer.java
+++ src/edu/towson/cis/cosc455/ftchen/project1/implementation/MyLexicalAnalyzer.java
@@ -4,28 +4,53 @@ import edu.towson.cis.cosc455.ftchen.project1.interfaces.LexicalAnalyzer;
 
 public class MyLexicalAnalyzer implements LexicalAnalyzer {
 
+	private int position = currentPosition;
+	
+	public void start(String s)
+	{
+		System.out.println(position);
+	}
+	
 	//@Override
 	public void getNextToken() {
 		// TODO Auto-generated method stub
-
+		getCharacter();
+		lookupToken();
 	}
 
 	//@Override
 	public void getCharacter() {
 		// TODO Auto-generated method stub
-
+		while (!isSpace(nextCharacter)) {
+			addCharacter();
+			//position++
+		}
+		//currentPosition++ so that it's not a space
 	}
 
 	//@Override
 	public void addCharacter() {
 		// TODO Auto-generated method stub
-
+		MyCompiler.nextToken = MyCompiler.nextToken + nextCharacter;	
 	}
 
 	//@Override
 	public boolean isSpace(String c) {
 		// TODO Auto-generated method stub
+		if (nextCharacter.equals(" ")){
+			return true;
+		}
 		return false;
+	}
+	
+	public boolean isSpecial(String c) {
+		if (c.equals("#") || c.equals("^") || c.equals("<") || c.equals("{") || c.equals("$") || c.equals("*") ||
+				c.equals("+") || c.equals("~") || c.equals("[") || c.equals("@") || c.equals("%"))
+			return true;
+		else {
+			return false;
+		}
+		
 	}
 
 	//@Override
