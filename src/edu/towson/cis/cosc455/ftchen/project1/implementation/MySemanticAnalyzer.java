33a508ee2a5e4267714bdee12cd863d487f4bd1e
fsxiaaa <ftchen1@students.towson.edu>
2015-11-12 08:05:53 -0500

    Semantic Analyzer works with multiple variable definitions.

--- src/edu/towson/cis/cosc455/ftchen/project1/implementation/MySemanticAnalyzer.java
+++ src/edu/towson/cis/cosc455/ftchen/project1/implementation/MySemanticAnalyzer.java
@@ -21,13 +21,18 @@ public class MySemanticAnalyzer {
 	 * Method to reorder the parseTree stack, resolving any variables/etc. along the way.
 	 */
 	public void reorderStack() {
-		try {
-			while ((MyCompiler.parseTree.size() > 0)) {
+		int variables = -1;
+		//try {
+			while (variables != 0)
+			{
+				variables = 0;
+			do {
 				System.out.println("[While]");
 				System.out.println("Working with: " + MyCompiler.parseTree.peek());
 				if (MyCompiler.parseTree.peek().equalsIgnoreCase(Tokens.USEB)) {
 					//"use" case
 					System.out.println("[use case]");
+					variables++;
 					if (scopeName.size() > 0) {
 						if (isDeclared(reordered.peek())) {
 							System.out.println(reordered.peek() + " equals " + scopeName.get(scopeName.size()-1));
@@ -35,7 +40,7 @@ public class MySemanticAnalyzer {
 					}
 					else {
 						System.out.println("variable not yet defined");
-						reordered.push(MyCompiler.parseTree.pop());
+						System.out.println(reordered.push(MyCompiler.parseTree.pop())); //push $USE
 					}
 				}
 				else if (MyCompiler.parseTree.peek().equalsIgnoreCase(Tokens.DEFUSEE)) {
@@ -50,6 +55,7 @@ public class MySemanticAnalyzer {
 						MyCompiler.parseTree.pop(); //pop =
 						scopeName.add(MyCompiler.parseTree.pop()); //name to scopeName
 						scopeValue.add(reordered.pop()); //value to scopeValue
+						reordered.pop(); //pop $END
 					}
 				}
 				else if (isEndToken(MyCompiler.parseTree.peek())) {
@@ -73,11 +79,11 @@ public class MySemanticAnalyzer {
 									MyCompiler.parseTree.push(scopeValue.get(scopeValue.size()-1)); //push variable value
 									reordered.pop(); //pop $END
 									System.out.println("variable replacement ok!");
+									variables--;
 								}
 								else {
-									MyCompiler.parseTree.push(reordered.pop()); //wrong variable. push $USE
-									MyCompiler.parseTree.push(reordered.pop()); //.. push variable name
-									MyCompiler.parseTree.push(reordered.pop()); //.. push $END
+									System.out.println("11111111" + MyCompiler.parseTree.push(Tokens.USEB)); //wrong variable. push $USE
+									System.out.println("22222222" +MyCompiler.parseTree.push(reordered.pop())); //.. push variable name
 									System.out.println("variable replacement not done!");
 								}
 							}
@@ -85,13 +91,14 @@ public class MySemanticAnalyzer {
 								System.out.println("entered else");
 								if (MyCompiler.parseTree.peek().equalsIgnoreCase(Tokens.PARAE)) {
 									scopeName.remove(scopeName.size()-1);
-									scopeValue.remove(scopeName.size()-1);
+									scopeValue.remove(scopeValue.size()-1);
 								}
 								MyCompiler.parseTree.push(reordered.pop()); //regular begin token. Push back to stack.
+								System.out.println("finished else");
 							}
 						}
 						while(MyCompiler.parseTree.size() > 0)
-							System.out.println(MyCompiler.parseTree.pop());
+								System.out.println(reordered.push(MyCompiler.parseTree.pop()));
 					}
 					else
 						reordered.push(MyCompiler.parseTree.pop()); //push begin token back to stack
@@ -101,15 +108,17 @@ public class MySemanticAnalyzer {
 					System.out.println("[text case]");
 					reordered.push(MyCompiler.parseTree.pop());
 				}
-			}
+				System.out.println("variables is: " + variables);
+			} while (!reordered.peek().equalsIgnoreCase(Tokens.DOCB));
+			if (variables != 0)
+				while (reordered.size() > 0)
+					MyCompiler.parseTree.push(reordered.pop());
 		}
-		catch (Exception e) {
-			System.out.println("Error. A semantic error has occured. Undefined variables attemped to be used.");
-			System.exit(1);
-		}
-		for (String s : scopeName) {
-			System.out.println("\n\n SN VALUE " + s);
-		}
+		//}
+		//catch (Exception e) {
+			//System.out.println("Error. A semantic error has occured. Undefined variables attemped to be used.");
+			//System.exit(1);
+		//}
 		System.out.println("\n\n REORDERED: ");
 		while(reordered.size() > 0)
 			System.out.println(reordered.pop());
@@ -192,271 +201,4 @@ public class MySemanticAnalyzer {
 		reorderStack();
 		return html;
 	}
-	
-	
-
-	/*
-	private int treePosition = 0;
-	private String value = "";
-	private String name = "";
-	private String nameValue = "";
-	private String html = "";
-	private ArrayList <String> variableNames = new ArrayList <String>();
-	private ArrayList <String> variableValues = new ArrayList <String>();
-	
-	public String generateHTML() {
-		System.out.println("generateHTML, treePosition: " + treePosition);
-		value = MyCompiler.parseTree.get(treePosition);
-		System.out.println("value = " + value);
-		
-		if (value.equalsIgnoreCase(Tokens.DOCB)) {
-			System.out.println("DOCB");
-			html += "<html>";
-			if (treePosition < MyCompiler.parseTree.size()) {
-				treePosition++;
-				generateHTML();
-			}
-		}
-		else if (value.equalsIgnoreCase(Tokens.DOCE)) {
-			System.out.println("DOCE");
-			html += "</html>";
-		}
-		else if (value.equalsIgnoreCase(Tokens.HEAD)) {
-			System.out.println("head");
-			html += "<head>";
-			if (treePosition < MyCompiler.parseTree.size()) {
-				treePosition++;
-				generateHTML();
-				if (treePosition < MyCompiler.parseTree.size()) {
-					treePosition++;
-					value = MyCompiler.parseTree.get(treePosition);
-					if (value.equalsIgnoreCase(Tokens.HEAD)) {
-						System.out.println("head2");
-						html += "</head>";
-						if (treePosition < MyCompiler.parseTree.size()) {
-							treePosition++;
-							generateHTML();
-						}
-					}
-				}
-			}
-		}
-		else if (value.equalsIgnoreCase(Tokens.TITLEB)) {
-			System.out.println("title");
-			html += "<title>";
-			if (treePosition < MyCompiler.parseTree.size()) {
-				treePosition++;
-				value = MyCompiler.parseTree.get(treePosition);
-				if (treePosition < MyCompiler.parseTree.size()) {
-					treePosition++;
-					generateHTML();
-					if (treePosition < MyCompiler.parseTree.size()) {
-						if (value.equalsIgnoreCase(Tokens.TITLEE)) {
-							System.out.println("title2");
-							html += "</title>";
-							if (treePosition < MyCompiler.parseTree.size()) {
-								treePosition++;
-								generateHTML();
-							}
-						}
-					}
-				}
-
-			}
-		}
-		else if (value.equalsIgnoreCase(Tokens.PARAB)) {
-			System.out.println("para");
-			html += "<p>";
-			if (treePosition < MyCompiler.parseTree.size()) {
-				treePosition++;
-				generateHTML();
-			}
-		}
-		else if (value.equalsIgnoreCase(Tokens.PARAE)) {
-			System.out.println("para2");
-			html += "</p>";
-			if (treePosition < MyCompiler.parseTree.size()) {
-				treePosition++;
-				generateHTML();
-			}
-		}
-		else if (value.equalsIgnoreCase(Tokens.BOLD)) {
-			System.out.println("bold");
-			html += "<b>";
-			if (treePosition < MyCompiler.parseTree.size()) {
-				treePosition++;
-				generateHTML();
-				if (treePosition < MyCompiler.parseTree.size()) {
-					treePosition++;
-					value = MyCompiler.parseTree.get(treePosition);
-					if (value.equalsIgnoreCase(Tokens.BOLD)) {
-						System.out.println("bold2");
-						html += "</b>";
-						if (treePosition < MyCompiler.parseTree.size()) {
-							treePosition++;
-							generateHTML();
-						}
-					}
-				}
-			}
-		}
-		else if (value.equalsIgnoreCase(Tokens.ITALICS)) {
-			System.out.println("italic");
-			html += "<i>";
-			if (treePosition < MyCompiler.parseTree.size()) {
-				treePosition++;
-				generateHTML();
-				if (treePosition < MyCompiler.parseTree.size()) {
-					treePosition++;
-					value = MyCompiler.parseTree.get(treePosition);
-					if (value.equalsIgnoreCase(Tokens.ITALICS)) {
-						System.out.println("italic2");
-						html += "</i>";
-						if (treePosition < MyCompiler.parseTree.size()) {
-							treePosition++;
-							generateHTML();
-						}
-					}
-				}
-			}
-		}
-		else if (value.equalsIgnoreCase(Tokens.LISTITEMB)) {
-			System.out.println("listitem");
-			html += "<li>";
-			if (treePosition < MyCompiler.parseTree.size()) {
-				treePosition++;
-				generateHTML();
-				if (treePosition < MyCompiler.parseTree.size()) {
-					treePosition++;
-					value = MyCompiler.parseTree.get(treePosition);
-					if (value.equalsIgnoreCase(Tokens.LISTITEME)) {
-						System.out.println("listitem2");
-						html += "</li>";
-						if (treePosition < MyCompiler.parseTree.size()) {
-							treePosition++;
-							generateHTML();
-						}
-					}
-				}
-			}
-		}
-		else if (value.equalsIgnoreCase(Tokens.NEWLINE)) {
-			System.out.println("newline");
-			html += "<br>";
-			if (treePosition < MyCompiler.parseTree.size()) {
-				treePosition++;
-				generateHTML();
-			}
-		}
-		else if (value.equalsIgnoreCase(Tokens.LINKB)) {
-			System.out.println("link");
-			html += "<a href=\" ";
-			if (treePosition < MyCompiler.parseTree.size()) {
-				treePosition++;
-				generateHTML();
-				if (treePosition < MyCompiler.parseTree.size()) {
-					treePosition++;
-					value = MyCompiler.parseTree.get(treePosition);
-					if (value.equalsIgnoreCase(Tokens.LINKE)) {
-						System.out.println("link2");
-						html += "</a>";
-						if (treePosition < MyCompiler.parseTree.size()) {
-							treePosition++;
-							generateHTML();
-						}
-					}
-				}
-			}
-		}
-		else if (value.equalsIgnoreCase(Tokens.ADDRESSB)) {
-			System.out.println("address");
-			if (treePosition < MyCompiler.parseTree.size()) {
-				treePosition++;
-				generateHTML();
-				if (treePosition < MyCompiler.parseTree.size()) {
-					treePosition++;
-					value = MyCompiler.parseTree.get(treePosition);
-					if (value.equalsIgnoreCase(Tokens.ADDRESSE)) {
-						System.out.println("address2");
-						html += "<\">";
-						if (treePosition < MyCompiler.parseTree.size()) {
-							treePosition++;
-							generateHTML();
-						}
-					}
-				}
-			}
-		}
-		else if (value.equalsIgnoreCase(Tokens.AUDIO)) {
-			System.out.println("audio");
-			html += "<audio controls> <source src=\"";
-			if (treePosition < MyCompiler.parseTree.size()) {
-				treePosition++;
-				generateHTML();
-			}
-			html += "< \"> </audio>>";
-		}
-		else if (value.equalsIgnoreCase(Tokens.VIDEO)) {
-			System.out.println("video");
-			html += "<iframe src=\" ";
-			if (treePosition < MyCompiler.parseTree.size()) {
-				treePosition++;
-				generateHTML();
-			}
-			html += "< \"/>";
-		}
-		else if (value.equalsIgnoreCase(Tokens.DEFB)) {
-			System.out.println("def");
-			System.out.println(value);
-			treePosition++;
-			value = MyCompiler.parseTree.get(treePosition);
-			name = value;
-			treePosition++;
-			treePosition++;
-			value = MyCompiler.parseTree.get(treePosition);
-			String nv = value;
-			System.out.println("n " + name);
-			value = MyCompiler.parseTree.get(treePosition);
-			System.out.println("v " + value);
-			if (treePosition < MyCompiler.parseTree.size()) {
-				treePosition++;
-				value = MyCompiler.parseTree.get(treePosition);
-				if (value.equalsIgnoreCase(Tokens.DEFUSEE)) {
-					if (treePosition < MyCompiler.parseTree.size()) {
-						treePosition++;
-						generateHTML();
-					}
-				}
-			}
-		}
-		else if (value.equalsIgnoreCase(Tokens.USEB)) {
-			System.out.println("use");
-			treePosition++;
-			value = MyCompiler.parseTree.get(treePosition);
-			if (value.equalsIgnoreCase(name))
-			{
-				System.out.println("cries");
-				html += nv;
-				if (treePosition < MyCompiler.parseTree.size()) {
-					treePosition++;
-					if (treePosition < MyCompiler.parseTree.size()) {
-						treePosition++;
-						generateHTML();
-					}
-				}
-			}
-		}
-		else if (Tokens.isText(value.substring(0,1))) {
-			System.out.println("Text");
-			html += value;
-			if (treePosition < MyCompiler.parseTree.size()) {
-				treePosition++;
-				generateHTML();
-			}
-		}
-		System.out.println(treePosition + " " + MyCompiler.parseTree.size());
-		System.out.println(html);
-		return html;
-	}
-	*/
 }
